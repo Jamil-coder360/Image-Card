a749942046da08beb2833e89f56fc3eb27ddcc36
Jamil-coder360 <jamil595nhj@gmail.com>
2026-04-22 00:27:09 +0600

    clear all main funcionality

--- src/component/MyCard.jsx
+++ src/component/MyCard.jsx
@@ -5,10 +5,17 @@ import { toast } from "react-toastify";
 
 const MyCard = () => {
   const fileRef = useRef();
-  const { item, addCard, deleteCard , deleteAll } = useContext(CardContext);
+  const editFileRef = useRef();
+  const { item, addCard, deleteCard, deleteAll, updateCard } =
+    useContext(CardContext);
   const [text, setText] = useState("");
   const [image, setImage] = useState("");
   const [title, setTitle] = useState("");
+  const [updateText, setUpdateText] = useState("");
+  const [updateTitle, setUpdateTitle] = useState("");
+  const [updateImage, setUpdateImage] = useState("");
+
+  const [edit, isEdit] = useState(false);
 
   const handleAdd = (e) => {
     e.preventDefault();
@@ -25,6 +32,25 @@ const MyCard = () => {
     if (file) {
       setImage(URL.createObjectURL(file));
     }
+  };
+    const handleEditImage = (e) => {
+    const file = e.target.files[0];
+    if (file) setUpdateImage(URL.createObjectURL(file));
+  };
+
+  const handleEdit = (id) => {
+    isEdit(id);
+    setText(item.find((item) => item.id === id).text);
+    setTitle(item.find((item) => item.id === id).title);
+    setImage(item.find((item) => item.id === id).image);
+  };
+  const handleSave = (id) => {
+    isEdit(false);
+     setUpdateText(text);
+    setUpdateTitle(title);
+    setUpdateImage(image);
+    updateCard(id, { id, text: updateText, title: updateTitle, image: updateImage });
+    toast.success("Todo updated successfully!");
   };
 
   return (
@@ -75,57 +101,104 @@ const MyCard = () => {
 
         {/* Cards Grid */}
         <div className="flex flex-col flex-wrap gap-6 justify-center mt-20">
-            <div className="flex items-center justify-between">
-                <h3 className="text-white text-3xl font-bold">total card : {item?.length || 0}</h3>
-                <Button onClick={deleteAll}>
-                    Delete All
-                </Button>
-            </div>
-            <div className="grid grid-cols-4 gap-6">
+          <div className="flex items-center justify-between">
+            <h3 className="text-white text-3xl font-bold">
+              total card : {item?.length || 0}
+            </h3>
+            <Button onClick={deleteAll}>Delete All</Button>
+          </div>
+          <div className="grid grid-cols-4 gap-6">
+            {item?.map((item, i) => (
+              <div
+                key={i}
+                className="w-72 rounded-2xl bg-white border border-gray-100 overflow-hidden cursor-pointer hover:-translate-y-2 transition-transform duration-200"
+              >
+                {/* Image */}
 
-          {item?.map((card, i) => (
-            <div
-              key={i}
-              className="w-72 rounded-2xl bg-white border border-gray-100 overflow-hidden cursor-pointer hover:-translate-y-2 transition-transform duration-200"
-            >
-              {/* Image */}
-              <div className="relative w-full h-48 bg-gradient-to-br from-blue-200 to-blue-400 overflow-hidden">
-                {card.image ? (
-                  <img
-                    src={card.image}
-                    alt={card.text}
-                    className="w-full h-full object-cover"
+                {edit === item.id ? (
+                  <input
+                    ref={editFileRef}
+                    type="file"
+                    className="w-full p-3 bg-gray-200 text-black rounded-xl outline-0"
+                    accept="image/*"
+                    onChange={handleEditImage}
                   />
                 ) : (
-                  <div className="w-full h-full bg-gradient-to-br from-blue-200 to-blue-400" />
+                  <div className="relative w-full h-48 bg-gradient-to-br from-blue-200 to-blue-400 overflow-hidden">
+                    {item.image ? (
+                      <img
+                        src={item.image}
+                        alt={item.text}
+                        className="w-full h-full object-cover"
+                      />
+                    ) : (
+                      <div className="w-full h-full bg-gradient-to-br from-blue-200 to-blue-400" />
+                    )}
+                  </div>
                 )}
-              </div>
 
-              {/* Body */}
-              <div className="px-5 py-5">
-                {/* Title */}
-                <h3 className="font-serif text-lg font-medium text-gray-900 leading-snug mb-2">
-                  {card.text || "Untitled"}
-                </h3>
+                {/* Body */}
+                <div className=" flex flex-col gap-4 px-5 py-5 ">
+                  {/* Title */}
 
-                {/* Description */}
-                <p className="text-sm text-gray-500 leading-relaxed mb-4">
-                  {card.title || "No description provided."}
-                </p>
+                  {edit === item.id ? (
+                    <input
+                      onChange={(e) => setUpdateText(e.target.value)}
+                      type="text"
+                      value={updateText}
+                      placeholder="Add a new title..."
+                      className="w-full p-3 bg-gray-200 text-black rounded-xl outline-0 "
+                    />
+                  ) : (
+                    <h3 className="font-serif text-lg font-medium text-gray-900 leading-snug mb-2">
+                      {item.text || "Untitled"}
+                    </h3>
+                  )}
 
-                {/* Footer */}
-                <div className="flex items-center justify-between">
-                  <Button className="text-xs  px-4 py-1.5 rounded-full hover:bg-gray-100 hover:text-black hover:border transition-colors">
-                    Edit
-                  </Button>
-                  <Button onClick={() => deleteCard(i)} className="text-xs bg-red-400 text-white px-4 py-1.5 rounded-full hover:bg-red-600 transition-colors">
-                    delete
-                  </Button>
+                  {/* Description */}
+                  {edit === item.id ? (
+                    <input
+                      onChange={(e) => setUpdateTitle(e.target.value)}
+                      type="text"
+                      value={updateTitle}
+                      placeholder="Add a new title..."
+                      className="w-full p-3 bg-gray-200 text-black rounded-xl outline-0"
+                    />
+                  ) : (
+                    <p className="text-sm text-gray-500 leading-relaxed mb-4">
+                      {item.title || "No description provided."}
+                    </p>
+                  )}
+
+                  {/* Footer */}
+                  <div className="flex items-center justify-between">
+                    {edit === item.id ? (
+                      <Button
+                        onClick={() => handleSave(item.id)}
+                        className="text-xs  px-4 py-1.5 rounded-full hover:bg-gray-100 hover:text-black hover:border transition-colors"
+                      >
+                        Save
+                      </Button>
+                    ) : (
+                      <Button
+                        onClick={() => handleEdit(item.id)}
+                        className="text-xs  px-4 py-1.5 rounded-full hover:bg-gray-100 hover:text-black hover:border transition-colors"
+                      >
+                        Edit
+                      </Button>
+                    )}
+
+                    <Button
+                    onClick={() => deleteCard(item.id)}
+                      className="text-xs bg-red-400 text-white px-4 py-1.5 rounded-full hover:bg-red-600 transition-colors"
+                    >
+                      delete
+                    </Button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-            </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
--- src/context/CardContext.jsx
+++ src/context/CardContext.jsx
@@ -1,23 +1,40 @@
 import { createContext, useState } from 'react';
+
 const CardContext = createContext();
 
 const CardProvider = ({ children }) => {
-    const [item ,setItem] = useState([]);
-    const addCard = (text,title,image) => {
-        // Logic to add a card to the context
-        setItem([...item, {text ,title, image}]);
-        console.log(item);
+  const [item, setItem] = useState([]);
+
+  const addCard = (text, title, image) => {
+    const newCard = {
+      id: Date.now(), // ✅ unique id
+      text,
+      title,
+      image,
     };
-    const deleteCard =(id) =>{
-        setItem(item.filter((_, i) => i !== id));
-    }
-    const deleteAll = ()=>{
-        setItem([]);
-    }
-    return (<CardContext.Provider value={{ item ,addCard, deleteCard , deleteAll}}>
-        {children}
-    </CardContext.Provider>
+    setItem((prev) => [...prev, newCard]); // ✅ prev state ব্যবহার করো
+  };
+
+  const updateCard = (id, updatedItem) => {
+    setItem((prev) =>
+      prev.map((card) => (card.id === id ? updatedItem : card))
     );
-;}
+  };
+
+  const deleteCard = (id) => {
+    setItem((prev) => prev.filter((card) => card.id !== id));
+  };
+
+  const deleteAll = () => {
+    setItem([]);
+  };
+
+  return (
+    <CardContext.Provider value={{ item, addCard, deleteCard, deleteAll, updateCard }}>
+      {children}
+    </CardContext.Provider>
+  );
+};
+
 export { CardContext };
 export default CardProvider;
